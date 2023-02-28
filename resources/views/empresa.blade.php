62ceef304b5c997ef7cbcf07d3afcf5a804c1c0d
gabigsc <gabrielagsc2005@gmail.com>
2023-02-28 10:45:08 -0300

    first commit

---
+++ resources/views/empresa.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Empresa</title>
+</head>
+<body>
+    <h1> Empresa </h1>
+</body>
+</html>
---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Produtos</title>
+</head>
+<body>
+    <h1> Produtos </h1>
+</body>
+</html>
